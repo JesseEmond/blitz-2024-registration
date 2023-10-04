c83f9d58264cf64570433e95fca2824c94840cda
JesseEmond <emond.jesse@gmail.com>
2023-10-04 19:55:08 -0400

    Aim at first meteor's current position

--- bot/bot.py
+++ bot/bot.py
@@ -1,22 +1,32 @@
+from typing import Optional
+
 from game_message import *
 from actions import *
 
 
 class Bot:
     def __init__(self):
-        self.direction = 1
-        print("Initializing your super mega duper bot")
+        pass
+
+    def pick_target(self, game: GameMessage) -> Optional[Meteor]:
+        if not game.meteors:
+            return None
+        # TODO: Pick target strategically (e.g. best score)
+        # TODO: Ignore meteors that we already shot at
+        # TODO: Take into accounts meteors that will spawn
+        return game.meteors[0]
+
+    def aim_at_target(self, meteor: Meteor) -> Vector:
+        # TODO: Predict collision point between our projectile & the meteor
+        return meteor.position
 
     def get_next_move(self, game_message: GameMessage):
-        """
-        Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-        """
-        if game_message.cannon.orientation >= 45:
-            self.direction = -1
-        elif game_message.cannon.orientation <= -45:
-            self.direction = 1
+        actions = []
 
-        return [
-            RotateAction(angle=15 * self.direction),
-            ShootAction(),
-        ]
+        target = self.pick_target(game_message)
+        if target:
+            actions.append(LookAtAction(target=self.aim_at_target(target)))
+            if not game_message.cannon.cooldown:
+                actions.append(ShootAction())
+
+        return actions
