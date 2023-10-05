7d7567c86e4c3da6ce4ecbe52a37ced8869ca930
JesseEmond <emond.jesse@gmail.com>
2023-10-04 20:19:37 -0400

    Aim ahead of target

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,29 @@
-from typing import Optional
+import math
+from typing import Optional, Tuple
 
 from game_message import *
 from actions import *
+
+
+def dot_product(a: Vector, b: Vector) -> float:
+    return a.x * b.x + a.y * b.y
+
+def vec_minus(a: Vector, b: Vector) -> Vector:
+    return Vector(a.x - b.x, a.y - b.y)
+
+def collision_times(delta_pos: Vector, delta_vel: Vector,
+                    speed: float) -> Optional[Tuple[float, float]]:
+    # https://gamedev.stackexchange.com/q/25277
+    a = dot_product(delta_vel, delta_vel) - speed * speed
+    b = 2 * dot_product(delta_vel, delta_pos)
+    c = dot_product(delta_pos, delta_pos)
+
+    p = -b / (2 * a)
+    det = b * b - 4 * a * c
+    if det < 0:
+        return None  # No solution
+    q = math.sqrt(det) / (2 * a)
+    return p - q, p + q
 
 
 class Bot:
@@ -16,17 +38,29 @@ class Bot:
         # TODO: Take into accounts meteors that will spawn
         return game.meteors[0]
 
-    def aim_at_target(self, meteor: Meteor) -> Vector:
-        # TODO: Predict collision point between our projectile & the meteor
-        return meteor.position
+    def aim_ahead(self, cannon: Cannon, rockets: RocketsConstants,
+                  target: Projectile) -> Optional[Vector]:
+        delta_pos = vec_minus(target.position, cannon.position)
+        times = collision_times(delta_pos, target.velocity, rockets.speed)
+        if not times:
+            return None
+        # TODO: Take into account that our target is a sphere of size > 0
+        # TODO: Take into account that our rocket is a sphere of size > 0
+        time = next(iter(sorted(t for t in times if t >= 0)), None)
+        if not time:
+            return None
+        return Vector(target.position.x + target.velocity.x * time,
+                      target.position.y + target.velocity.y * time)
 
-    def get_next_move(self, game_message: GameMessage):
+
+    def get_next_move(self, game: GameMessage):
         actions = []
 
-        target = self.pick_target(game_message)
+        target = self.pick_target(game)
         if target:
-            actions.append(LookAtAction(target=self.aim_at_target(target)))
-            if not game_message.cannon.cooldown:
+            aim = self.aim_ahead(game.cannon, game.constants.rockets, target)
+            actions.append(LookAtAction(target=aim))
+            if not game.cannon.cooldown:
                 actions.append(ShootAction())
 
         return actions
