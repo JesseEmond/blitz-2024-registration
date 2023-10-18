935bd117bf01c17ff097f110615fcb8cb6f8ac2b
JesseEmond <emond.jesse@gmail.com>
2023-10-18 01:11:18 -0400

    More disassembly noise removal for checksums/diffs

--- disassembled_js/remove_disassembly_noise.sh
+++ disassembled_js/remove_disassembly_noise.sh
@@ -4,4 +4,9 @@
 
 # - Replace exact (dynamic) addresses like 0x1234567890 with 'ADDR'
 # - Replace property pointer values such as "const data descriptor, p: 6" with PTR
-sed 's/0x[0-9a-f]\{8,16\}/ADDR/g' | sed 's/descriptor, p: [0-9]\+/descriptor, p: PTR/g'
+# - Remove source and bytecode offset information before the bytecode bytes.
+#   They make diffs harder to parse, and the bytecode difference, if any, will
+#   already impact checksums.
+sed 's/0x[0-9a-f]\{8,16\}/ADDR/g' |\
+sed 's/descriptor, p: [0-9]\+/descriptor, p: PTR/g' |\
+sed 's/^.*[0-9]\+ :/LOC/g'
