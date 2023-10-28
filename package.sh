f1a379f703036412f4d72d7481836c6e0f11cb52
JesseEmond <emond.jesse@gmail.com>
2023-10-28 00:04:22 -0400

    Don't package release files

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -r bot.zip bot -x 'bot/__pycache__/**' -x '*.swp' -x 'bot/.pytype/**' -x 'bot/target/debug/**' -x 'bot/target/.rustc_info.json'
+zip -r bot.zip bot -x 'bot/__pycache__/**' -x '*.swp' -x 'bot/.pytype/**' -x 'bot/target/debug/**' -x 'bot/target/release/**' -x 'bot/target/.rustc_info.json'
