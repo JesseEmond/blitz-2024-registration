faaca40739faabc82d878f22a540be2392491fec
JesseEmond <emond.jesse@gmail.com>
2023-10-29 16:16:33 -0400

    Expand exclusions from packaging script

--- package.sh
+++ package.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-zip -r bot.zip bot -x 'bot/__pycache__/**' -x '*.swp' -x 'bot/.pytype/**' -x 'bot/target/debug/**' -x 'bot/target/release/**' -x 'bot/target/.rustc_info.json'
+zip -r bot.zip bot \
+    -x 'bot/__pycache__/**' -x '*.swp' -x 'bot/.pytype/**' \
+    -x 'bot/target/debug/**' -x 'bot/target/release/**' \
+    -x 'bot/target/.rustc_info.json' \
+    -x 'bot/target/criterion/**' \
+    -x 'bot/target/tmp/**' \
+    -x 'bot/perf.data*' -x 'bot/flamegraph.svg'
